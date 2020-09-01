d75a4bd0dccbc7d4191e57f4e4e6b77577b69eb1
Martin Tierro <martintierro15@gmail.com>
2020-09-01 18:01:26 +0800

    Connected reset passwords for admin and secretary to back-end

--- src/components/admin/module/admin-reset-password-modal.js
+++ src/components/admin/module/admin-reset-password-modal.js
@@ -1,8 +1,16 @@
 import React from "react";
+import axios from 'axios';
 import {Modal, Icon, Popup, Button,  Form, Input} from 'semantic-ui-react'
+import { toast } from "react-semantic-toasts";
 
 
 class AdminResetPasswordModal extends React.Component{
+
+    state = {
+        currentPassword:'',
+        newPassword: '',
+        confirmNewPassword: '',
+    }
 
     handleOpen = () => this.props.handleModal("admin-reset-password");
 
@@ -11,6 +19,30 @@ class AdminResetPasswordModal extends React.Component{
     handleModal(name){
         this.props.handleModal(name)
     }
+
+    handleSubmit = event =>{
+        event.preventDefault()
+        const data = {
+            username: "admin",
+            newPassword: this.state.newPassword  
+        }
+        axios.post('admin/updateAccountPassword', data)
+            .then(res=>{
+                console.log(res)
+                console.log(res.data)
+            })
+        this.handleClose()
+        setTimeout(() =>{
+            toast({
+                type: "success",
+                title: 'Success',
+                description: <p>"Password successfully reset"</p>,
+                icon: "check"
+            })
+        }, 1000)
+    }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
     render(){
         let open
@@ -36,15 +68,15 @@ class AdminResetPasswordModal extends React.Component{
                 </Modal.Header>
 
                 <Modal.Content>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Field required id="current-password-field" >
                             <label>Current Password</label>
-                            <Input name="currentPassword" type="password" id="current-password" autoComplete="false" placeholder="Confirm Password" />
+                            <Input name="currentPassword" type="password" id="current-password" autoComplete="false" placeholder="Confirm Password" onChange={this.handleChange}/>
                         </Form.Field>
                         <Form.Field required id="new-password-field" >
                             <label>New Password</label>
                             <Popup
-                                trigger={<Input name="newPassword" type="password" id="new-password" autoComplete="false" placeholder="New Password" />}
+                                trigger={<Input name="newPassword" type="password" id="new-password" autoComplete="false" placeholder="New Password" onChange={this.handleChange} />}
                                 content='Password should contain 10 to 32 alphanumeric characters'
                                 position='right center'
                                 />
@@ -52,7 +84,7 @@ class AdminResetPasswordModal extends React.Component{
                         <Form.Field required id="confirm-new-password-field">
                             <label>Confirm New Password</label>
                             <Popup
-                                trigger={<Input name="confirmNewPassword" type="password"id="confirm-new-password" autoComplete="false" placeholder="Confirm New Password" />}
+                                trigger={<Input name="confirmNewPassword" type="password"id="confirm-new-password" autoComplete="false" placeholder="Confirm New Password" onChange={this.handleChange} />}
                                 content='Password should contain 10 to 32 alphanumeric characters'
                                 position='right center'
                                 />
@@ -62,7 +94,7 @@ class AdminResetPasswordModal extends React.Component{
 
 
                 <Modal.Actions>
-                    <Button icon labelPosition='left' color="green" id="save-password">
+                    <Button icon labelPosition='left' color="green" id="save-password"  onClick={this.handleSubmit}>
                         <Icon name='check' />
                         CONFIRM
                     </Button>
--- src/components/admin/module/admin-reset-secretary-modal.js
+++ src/components/admin/module/admin-reset-secretary-modal.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from 'axios';
 import {Modal, Icon, Popup, Button,  Form, Input} from 'semantic-ui-react'
-
+import { toast } from "react-semantic-toasts";
 
 class AdminResetSecretaryModal extends React.Component{
 
+    state = {
+        currentPassword:'',
+        newPassword: '',
+        confirmNewPassword: '',
+    }
     handleOpen = () => this.props.handleModal("admin-reset-secretary");
 
     handleClose = () => this.props.handleModal('none')
@@ -11,6 +17,30 @@ class AdminResetSecretaryModal extends React.Component{
     handleModal(name){
         this.props.handleModal(name)
     }
+
+    handleSubmit = event =>{
+        event.preventDefault()
+        const data = {
+            username: "secretary",
+            newPassword: this.state.newPassword  
+        }
+        axios.post('admin/updateAccountPassword', data)
+            .then(res=>{
+                console.log(res)
+                console.log(res.data)
+            })
+        this.handleClose()
+        setTimeout(() =>{
+            toast({
+                type: "success",
+                title: 'Success',
+                description: <p>"Password successfully reset"</p>,
+                icon: "check"
+            })
+        }, 1000)
+    }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
     render(){
         let open
@@ -36,15 +66,15 @@ class AdminResetSecretaryModal extends React.Component{
                 </Modal.Header>
 
                 <Modal.Content>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Field required id="sec-current-password-field" >
                             <label>Current Password</label>
-                            <Input name="currentPassword" type="password" id="sec-current-password" autoComplete="false" placeholder="Confirm Password" />
+                            <Input name="currentPassword" type="password" id="sec-current-password" autoComplete="false" placeholder="Confirm Password" onChange={this.handleChange}/>
                         </Form.Field>
                         <Form.Field required id="sec-new-password-field" >
                             <label>New Password</label>
                             <Popup
-                                trigger={<Input name="newPassword" type="password" id="sec-new-password" autoComplete="false" placeholder="New Password" />}
+                                trigger={<Input name="newPassword" type="password" id="sec-new-password" autoComplete="false" placeholder="New Password" onChange={this.handleChange}/>}
                                 content='Password should contain 10 to 32 alphanumeric characters'
                                 position='right center'
                                 />
@@ -52,7 +82,7 @@ class AdminResetSecretaryModal extends React.Component{
                         <Form.Field required id="sec-confirm-new-password-field">
                             <label>Confirm New Password</label>
                             <Popup
-                                trigger={<Input name="confirmNewPassword" type="password"id="sec-confirm-new-password" autoComplete="false" placeholder="Confirm New Password" />}
+                                trigger={<Input name="confirmNewPassword" type="password"id="sec-confirm-new-password" autoComplete="false" placeholder="Confirm New Password" onChange={this.handleChange} />}
                                 content='Password should contain 10 to 32 alphanumeric characters'
                                 position='right center'
                                 />
@@ -62,7 +92,7 @@ class AdminResetSecretaryModal extends React.Component{
 
 
                 <Modal.Actions>
-                    <Button icon labelPosition='left' color="green" id="sec-save-password">
+                    <Button icon labelPosition='left' color="green" id="sec-save-password" onClick={this.handleSubmit}>
                         <Icon name='check' />
                         CONFIRM
                     </Button>
