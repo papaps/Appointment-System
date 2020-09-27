b37d2ebcb2f208a1c968bb3ffa4bace957ba987f
Jeff <earl_capistrano@dlsu.edu.ph>
2020-09-27 16:30:45 +0800

    test

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var app = new express();
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static('build'));
+app.use('/', express.static(path.join(__dirname, '/build')));
+
 
 mongoose.Promise = global.Promise
 
