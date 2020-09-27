7f3e9940281d41b88a74933c2ee1d35cda8d3520
Jeff <earl_capistrano@dlsu.edu.ph>
2020-09-27 19:04:33 +0800

    another test

--- server.js
+++ server.js
@@ -14,22 +14,8 @@ const port = process.env.PORT || 5000;
 
 var app = new express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-if(process.env.NODE_ENV === 'production') {  
-    app.use(express.static(path.join(__dirname, 'client/build')));  
-    app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
-}
-
-app.get('*', (req, res) => {  
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
-
 app.use(cors());
 app.use(express.json())
-
-app.use('/', express.static(path.join(__dirname, '/build')));
-
 
 mongoose.Promise = global.Promise
 
